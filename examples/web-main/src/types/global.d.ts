7173fea356089cb28c99449433b255803d9af655
LiChen <2446432344@qq.com>
2023-02-02 18:46:48 +0800

    style: 完善文件结构

---
+++ examples/web-main/src/types/global.d.ts
@@ -0,0 +1,10 @@
+// 随机事件数据类型
+declare interface randomEvent {
+  eventContent: string;
+  firstChoiceContent: string;
+  firstChoiceResult: string;
+  firstChoiceChange: number[];
+  secondChoiceContent: string;
+  secondChoiceResult: string;
+  secondChoiceChange: number[];
+}
--- examples/web-main/src/views/game/Index.vue
+++ examples/web-main/src/views/game/Index.vue
@@ -46,6 +46,7 @@
 <script lang="ts">
 import { ref, reactive } from "vue";
 import { useRouter } from "vue-router";
+import { getRandomEvent } from "@/utils/game";
 
 export default {
   name: "start",
@@ -158,63 +159,6 @@ export default {
       updateRandomEvent();
       updateFrontCurEvent();
       flipReverseCard();
-    }
-
-    // 随机事件数据类型
-    type randomEvent = {
-      eventContent: string;
-      firstChoiceContent: string;
-      firstChoiceResult: string;
-      firstChoiceChange: number[];
-      secondChoiceContent: string;
-      secondChoiceResult: string;
-      secondChoiceChange: number[];
-    };
-
-    // 获取随机事件
-    function getRandomEvent(week: number): randomEvent {
-      // 随机事件
-      const randomEventsFrom1To16: randomEvent[] = [
-        {
-          eventContent: "test1",
-          firstChoiceContent: "yes",
-          firstChoiceResult: "yes",
-          firstChoiceChange: [0, 0, 0, 0, 0],
-          secondChoiceContent: "no",
-          secondChoiceResult: "no",
-          secondChoiceChange: [0, 0, 0, 0, 0],
-        },
-        {
-          eventContent: "test2",
-          firstChoiceContent: "yes",
-          firstChoiceResult: "yes",
-          firstChoiceChange: [0, 0, 0, 0, 0],
-          secondChoiceContent: "no",
-          secondChoiceResult: "no",
-          secondChoiceChange: [0, 0, 0, 0, 0],
-        },
-        {
-          eventContent: "test3",
-          firstChoiceContent: "yes",
-          firstChoiceResult: "yes",
-          firstChoiceChange: [0, 0, 0, 0, 0],
-          secondChoiceContent: "no",
-          secondChoiceResult: "no",
-          secondChoiceChange: [0, 0, 0, 0, 0],
-        },
-      ];
-
-      let randomEvents: randomEvent[];
-      if (week <= 4) {
-        randomEvents = randomEventsFrom1To16;
-      } else if (week <= 10) {
-        randomEvents = randomEventsFrom1To16;
-      } else if (week <= 12) {
-        randomEvents = randomEventsFrom1To16;
-      } else {
-        randomEvents = randomEventsFrom1To16;
-      }
-      return randomEvents[Math.floor(Math.random() * randomEvents.length)];
     }
 
     return {
