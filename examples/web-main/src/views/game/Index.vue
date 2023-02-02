0a3f374daa94c1ee3ed8aa818433267ca6489c30
LiChen <2446432344@qq.com>
2023-02-02 17:16:28 +0800

    feat: 简单实现游戏选择逻辑

--- examples/web-main/src/views/game/Index.vue
+++ examples/web-main/src/views/game/Index.vue
@@ -2,26 +2,47 @@
   <div class="game">
     <!-- 属性区 -->
     <div class="game-attribute-bar">
-      <div class="game-attribute time">时间</div>
-      <div class="game-attribute">体力</div>
-      <div class="game-attribute">心理</div>
-      <div class="game-attribute">资金</div>
+      <div class="time">时间：{{ attribute.week }}</div>
+      <div class="game-attribute">体力：{{ attribute.health }}</div>
+      <div class="game-attribute">心理：{{ attribute.heart }}</div>
+      <div class="game-attribute">资金：{{ attribute.money }}</div>
     </div>
 
     <!-- 游戏区 -->
     <div class="game-play">
-      <button @click="backStart">返回开始界面</button>
+      <!-- 反面卡片 -->
+      <div ref="reverseCard" class="random-event card reverse-card">
+        <div class="random-event-result">结果：{{ curEvent.eventResult }}</div>
+        <button @click="continueGame">继续</button>
+      </div>
+
+      <!-- 正面卡片 -->
+      <div ref="frontCard" class="random-event card">
+        <div class="random-event-content">
+          事件：{{ curEvent.eventContent }}
+        </div>
+
+        <button @click="firstChoice" class="choice-content">
+          选项一：{{ curEvent.firstChoiceContent }}
+        </button>
+
+        <button @click="secondChoice" class="choice-content">
+          选项二：{{ curEvent.secondChoiceContent }}
+        </button>
+      </div>
     </div>
+    <button @click="backStart">返回开始界面</button>
 
     <!-- 事件区 -->
     <div class="game-event-bar">
-      <div class="game-event">学习事件</div>
-      <div class="game-event">恋爱事件</div>
+      <div class="game-event">学习事件：{{ attribute.grades }}</div>
+      <div class="game-event">恋爱事件：{{ attribute.love }}</div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import { ref, reactive, onMounted } from "vue";
 import { useRouter } from "vue-router";
 
 export default {
@@ -31,10 +52,164 @@ export default {
     const router = useRouter();
 
     // 返回开始页面
-    const backStart = () => {
+    function backStart() {
       router.push("/start");
+    }
+
+    // 属性值
+    const attribute = reactive({
+      week: 0,
+      health: 0,
+      heart: 0,
+      money: 0,
+      grades: 0,
+      love: 0,
+    });
+
+    // 当前显示事件
+    const curEvent = reactive({
+      eventContent: "",
+      firstChoiceContent: "",
+      secondChoiceContent: "",
+      eventResult: "",
+    });
+
+    // 随机事件
+    let randomEvent: randomEvent = {
+      eventContent: "游戏玩法简介",
+      firstChoiceContent: "我看懂了。",
+      firstChoiceResult: "游玩愉快。",
+      firstChoiceChange: [0, 0, 0, 0, 0],
+      secondChoiceContent: "我没看懂。",
+      secondChoiceResult: "玩着玩着就懂了。",
+      secondChoiceChange: [0, 0, 0, 0, 0],
     };
-    return { backStart };
+
+    initGame();
+
+    // 游戏参数初始化
+    function initGame() {
+      attribute.week = 0;
+      attribute.health = 20;
+      attribute.heart = 20;
+      attribute.money = 20;
+      attribute.grades = 20;
+      attribute.love = 20;
+      updateCurEvent();
+    }
+
+    // 更新当前事件内容
+    function updateCurEvent() {
+      curEvent.eventContent = randomEvent.eventContent;
+      curEvent.firstChoiceContent = randomEvent.firstChoiceContent;
+      curEvent.secondChoiceContent = randomEvent.secondChoiceContent;
+    }
+
+    let frontCard = ref();
+    let reverseCard = ref();
+
+    // 正面卡片翻转
+    function flipFrontCard() {
+      frontCard.value.style.transform = "rotateY(180deg)";
+      reverseCard.value.style.transform = "rotateY(0deg)";
+    }
+
+    // 反面卡片翻转
+    function flipReverseCard() {
+      reverseCard.value.style.transform = "rotateY(-180deg)";
+      frontCard.value.style.transform = "rotateY(0deg)";
+    }
+
+    // 选项一
+    function firstChoice() {
+      console.log("firstChoice");
+      curEvent.eventResult = randomEvent.firstChoiceResult;
+      console.log(curEvent.eventResult);
+      flipFrontCard();
+    }
+
+    // 选项二
+    function secondChoice() {
+      console.log("secondChoice");
+      curEvent.eventResult = randomEvent.secondChoiceResult;
+      console.log(curEvent.eventResult);
+      flipFrontCard();
+    }
+
+    // 继续游戏
+    function continueGame() {
+      randomEvent = getRandomEvent(attribute.week);
+      updateCurEvent();
+      flipReverseCard();
+    }
+
+    // 随机事件数据类型
+    type randomEvent = {
+      eventContent: string;
+      firstChoiceContent: string;
+      firstChoiceResult: string;
+      firstChoiceChange: number[];
+      secondChoiceContent: string;
+      secondChoiceResult: string;
+      secondChoiceChange: number[];
+    };
+
+    // 获取随机事件
+    function getRandomEvent(week: number): randomEvent {
+      // 随机事件
+      const randomEventsFrom1To16: randomEvent[] = [
+        {
+          eventContent: "test1",
+          firstChoiceContent: "yes",
+          firstChoiceResult: "yes",
+          firstChoiceChange: [0, 0, 0, 0, 0],
+          secondChoiceContent: "no",
+          secondChoiceResult: "no",
+          secondChoiceChange: [0, 0, 0, 0, 0],
+        },
+        {
+          eventContent: "test2",
+          firstChoiceContent: "yes",
+          firstChoiceResult: "yes",
+          firstChoiceChange: [0, 0, 0, 0, 0],
+          secondChoiceContent: "no",
+          secondChoiceResult: "no",
+          secondChoiceChange: [0, 0, 0, 0, 0],
+        },
+        {
+          eventContent: "test3",
+          firstChoiceContent: "yes",
+          firstChoiceResult: "yes",
+          firstChoiceChange: [0, 0, 0, 0, 0],
+          secondChoiceContent: "no",
+          secondChoiceResult: "no",
+          secondChoiceChange: [0, 0, 0, 0, 0],
+        },
+      ];
+
+      let randomEvents: randomEvent[];
+      if (week <= 4) {
+        randomEvents = randomEventsFrom1To16;
+      } else if (week <= 10) {
+        randomEvents = randomEventsFrom1To16;
+      } else if (week <= 12) {
+        randomEvents = randomEventsFrom1To16;
+      } else {
+        randomEvents = randomEventsFrom1To16;
+      }
+      return randomEvents[Math.floor(Math.random() * randomEvents.length)];
+    }
+
+    return {
+      backStart,
+      attribute,
+      curEvent,
+      frontCard,
+      reverseCard,
+      firstChoice,
+      secondChoice,
+      continueGame,
+    };
   },
 };
 </script>
@@ -47,10 +222,16 @@ export default {
   .game-attribute-bar {
     flex: 1;
     display: flex;
-    .game-attribute {
+    .time {
       flex: 1;
       display: flex;
-      flex-flow: column;
+      align-items: center;
+      justify-content: center;
+      border: 1px solid #ccc;
+    }
+    .game-attribute {
+      flex: 2;
+      display: flex;
       align-items: center;
       justify-content: center;
       border: 1px solid #ccc;
@@ -59,15 +240,43 @@ export default {
   .game-play {
     flex: 5;
     display: flex;
-    flex-flow: column;
     align-items: center;
     justify-content: center;
     border: 1px solid #ccc;
     height: 100%;
-    .game-title {
-      margin: 10px;
-      font-size: xx-large;
-      font-weight: bolder;
+    .random-event {
+      display: flex;
+      flex-flow: column;
+      align-items: center;
+      justify-content: center;
+      border: 1px solid #ccc;
+      border-radius: 10px;
+      width: 600px;
+      height: 300px;
+      background-color: #eee;
+      .random-event-content {
+        flex: 1;
+        display: flex;
+        flex-flow: column;
+        align-items: center;
+        justify-content: center;
+        margin: 5% 5%;
+      }
+      .choice-content {
+        flex: 1;
+        display: flex;
+        flex-flow: column;
+        align-items: center;
+        justify-content: center;
+        margin: 0 5% 5%;
+        width: 80%;
+      }
+      .random-event-result {
+        display: flex;
+        flex-flow: column;
+        align-items: center;
+        justify-content: center;
+      }
     }
   }
   .game-event-bar {
@@ -82,5 +291,16 @@ export default {
       border: 1px solid #ccc;
     }
   }
+}
+
+// 卡片翻转效果
+.card {
+  position: absolute;
+  backface-visibility: hidden;
+  transition: all 1s;
+}
+
+.reverse-card {
+  transform: rotateY(-180deg);
 }
 </style>
